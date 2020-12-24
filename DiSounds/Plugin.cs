fce42d8303a65224519f55086604fc8d18682d1b
Auros Nexus <aurosnex@gmail.com>
2020-12-23 23:07:31 -0500

    Add logging

--- DiSounds/Plugin.cs
+++ DiSounds/Plugin.cs
@@ -1,15 +1,20 @@
 ﻿using IPA;
+using SiraUtil;
+using SiraUtil.Zenject;
+using SiraUtil.Attributes;
 using IPALogger = IPA.Logging.Logger;
 
 namespace DiSounds
 {
-    [Plugin(RuntimeOptions.DynamicInit)]
+    [Plugin(RuntimeOptions.DynamicInit), Slog]
     public class Plugin
     {
         [Init]
-        public Plugin(IPALogger _)
+        public Plugin(IPALogger log, Zenjector zenjector)
         {
-
+            zenjector
+                .On<PCAppInit>()
+                .Pseudo(Container => Container.BindLoggerAsSiraLogger(log));
         }
 
         [OnEnable, OnDisable]
